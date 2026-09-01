fd94a1607eb24b5437206653b3d856c12a536d84
AyeshaNadeemAkhtar <ayshanadeemakhtar@gmail.com>
2026-09-01 12:08:36 +0500

    Write translate.py

--- vocab.io-backend/app.py
+++ vocab.io-backend/app.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS # Cross Origin Resource Sharing. React port and Flask port can connect.
 from utils.sanitize import sanitize_text
 from llm_extract import get_best_keywords
+from translate import get_meanings
 
 app = Flask(__name__)
-CORS(app) # Allow React dev server(different port) to call this backend.
+CORS(app) # Allow React dev server(different port 5173) to call this backend.(port 5000)
 
 @app.route('/api/keywords', methods=["POST"])
 def extract_keywords_endpoint():
@@ -22,8 +23,11 @@ def extract_keywords_endpoint():
     #TODO: GEMINI call
     keywords = get_best_keywords(clean_text, language=language)
 
+    # Meanings
+    results = get_meanings(keywords, source_lang=language)
+
     # {"keywords" : ["nuove", "piacere", "buonaserra"]}
-    return jsonify({"keywords": keywords})
+    return jsonify({"keywords": results})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- vocab.io-backend/translate.py
+++ vocab.io-backend/translate.py
@@ -1,4 +1,6 @@
-import argotranslate.translate
+import argostranslate.translate
+import json
+from llm_extract import client
 
 LANG_CODE_MAP = {
     "italian": "it",
@@ -7,7 +9,59 @@ LANG_CODE_MAP = {
     "english": "en",
 }
 
+def get_simple_definitions_for_english(words: list) -> dict:
+    prompt = f"""Give a simple, one-sentence definition for each of these english words
+    for a language learner. If a word is archaic/old English, mention that.
+    Words: {words}
+    Return ONLY a JSON object mapping each word to its definition.
+    Example: {{"word1": "definition1", "word2": "definition2"}}"""
+
+    response = client.models.generate_content(
+        model="gemini-3.6-flash",
+        contents=prompt,
+    )
+
+    # response.text = 
+    # " ```json\n
+    # ["gatto, "correre"]\n
+    # ```  "
+    raw = response.text.strip() # remove spaces from start and end of whole response.text
+
+    if raw.startswith("```"):
+
+        # Chew off backtick characters from both sides, 
+        # replace the first occurence of "json" with nothing
+        # with strip() remove the newlines 
+        raw = raw.strip("`").replace("json", "", 1).strip() # ["gatto", "correra"] => text
+    
+    # load json string to python list object
+    return json.loads(raw)
+
+
+
+
+
 def get_meanings(keywords: list, source_lang: str = "english"):
 
     # get always get the value based on key get("italian") => "it"
     src_code = LANG_CODE_MAP.get(source_lang.lower())
+
+    if not src_code:
+        # [ LIST COMPREHENSION 
+        # {"word": "excusez", "meaning": "Unsupported Language"},
+        # {"word": "hej", "meaning": "Unsupported Language"}
+        #]
+        return [{"word": kw, "meaning": "(Unsupported Language)"} for kw in keywords]
+
+    results = []
+    for kw in keywords:
+        try:
+            if src_code == "en":
+                meaning = get_simple_definitions_for_english(keywords)
+            else:
+                meaning = argotranslate.translate.translate(kw, src_code, "en")
+        except Exception:
+            meaning = "(translation unavailable)"
+        results.append({"word": kw, "meaning": meaning})
+
+    return results
